16c14de715bb35aca89acb5bd86c96912d4122a3
getNotesFS <sfmarmolg@gmail.com>
2023-12-12 14:05:40 +0100

    last changes

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -4,14 +4,13 @@ import intheends from './music-player/sngs/In The End - Linkin Park.mp3';
 import faints from './music-player/sngs/Faint - Linkin Park.mp3'; 
 import disfigs from './music-player/sngs/Disfigure - Blank [NCS Release].mp3';
 // import bfmvs from './music-player/sngs/Bullet For My Valentine - Tears Don_t Fall.mp3';
-import blinkas from './music-player/sngs/blink-182 - Adam-s Song.mp3';
+import blinkas from './music-player/sngs/blk182.mp3';
 
 // audio thumbnails
 import intheend from './music-player/imgs/In The End - Linkin Park.jpg';
 import faint from './music-player/imgs/Fain - Linkin Park.jpg'; 
-import disfigure from './music-player/imgs/disfigure.jpg';
-// import bfmv from './music-player/imgs/Bullet For My Valentine - Scream Aim Fire.jpg';
-import blinka from './music-player/imgs/blink-182 - Adam-s Song.jpeg';
+import disfigure from './music-player/imgs/disfigure.jpg'; 
+import blinka from './music-player/imgs/blk182.jpeg';
 
 export  const tracks = [
   {
