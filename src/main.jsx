59724bf6919e3291068cbedc897dbfade088bf60
getNotesFS <sfmarmolg@gmail.com>
2023-12-14 04:54:09 +0100

    with axe

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import reportAccessibility from './components/utils/reportAccessibility'
 import { BrowserRouter } from "react-router-dom"
 import App from './App.jsx'
 import './index.css' 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+// ) 
+ 
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+)
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+ 
+reportAccessibility(React)
