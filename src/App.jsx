a904cd81a5595eb4620c2c64cca6861e953e1c75
getNotesFS <sfmarmolg@gmail.com>
2023-12-14 01:29:37 +0100

    test with ant

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { useState } from 'react';
 import './App.css';
+
+import {
+  Layout
+
+} from 'antd';
+
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
@@ -22,7 +28,7 @@ import Search from './components/Search';
 import database from './data/search-data';
 import { dataFotografia, dataSenderismo, dataVideojuegos } from './data/search-data';
 
- 
+
 const ScrollToTop = () => {
   const location = useLocation();
 
@@ -34,74 +40,83 @@ const ScrollToTop = () => {
 };
 
 function App() {
- 
+
 
   const [searchVisible, setSearchVisible] = useState(false);
+  let {Content} = Layout;
 
- 
   return (
-    <Router>
-     <ScrollToTop />
-      <Header setSearchVisible={setSearchVisible} />
-      <div>
-        {searchVisible && <Search data={database.hobbies} setSearchVisible={setSearchVisible} />}
-      </div>
-      <main>
- 
 
-        <Routes>
-         
-        <Route path="/" index element={<Home />} />
-          <Route path="/musica" element={<Music />} />
-          <Route path="/hobbies" element={<Hobbies />} />
-          <Route path="/peliculas" element={<Movie />} />
-          <Route path="/sobre-mi" element={<AboutMe />} />
-
-          <Route path="/hobbies/fotografia" element={
+    <Layout className="layout" style={{ minHeight: "100vh" }}>
 
 
-            <HobbiesPost
-              title={dataFotografia.title}
-              content={dataFotografia.content}
-              img_url={dataFotografia.img_url}
-              video_url_pg={dataFotografia.video_url_pg}
-              arial_label={dataFotografia.arial_label}
-              galleryItems={dataFotografia.galleryItems}
-              nextLink={dataFotografia.nextLink}
-              previousLink={dataFotografia.previousLink}
-            />
-          } />
+      <Content>
+        <div className="site-layout-content">
+          <ScrollToTop />
+          <Header setSearchVisible={setSearchVisible} />
+          <main>
+            <div className='search_main_container'>
+              {searchVisible && <Search data={database.hobbies} setSearchVisible={setSearchVisible} />}
+            </div>
+            <Routes>
 
-          <Route path="/hobbies/senderismo" element={
-            
-              <HobbiesPost
-                title={dataSenderismo.title}
-                content={dataSenderismo.content}
-                img_url={dataSenderismo.img_url}
-                video_url_pg={dataSenderismo.video_url_pg}
-                arial_label={dataSenderismo.arial_label}
-                galleryItems={dataSenderismo.galleryItems}
-                nextLink={dataSenderismo.nextLink}
-                previousLink={dataSenderismo.previousLink}
-              /> 
-          } />
+              <Route path="/" index element={<Home />} />
+              <Route path="/musica" element={<Music />} />
+              <Route path="/hobbies" element={<Hobbies />} />
+              <Route path="/peliculas" element={<Movie />} />
+              <Route path="/sobre-mi" element={<AboutMe />} />
 
-          <Route path="/hobbies/videojuegos" element={<HobbiesPost
-            title={dataVideojuegos.title}
-            content={dataVideojuegos.content}
-            img_url={dataVideojuegos.img_url}
-            video_url_pg={dataVideojuegos.video_url_pg}
-            arial_label={dataVideojuegos.arial_label}
-            galleryItems={dataVideojuegos.galleryItems}
-            nextLink={dataVideojuegos.nextLink}
-            previousLink={dataVideojuegos.previousLink}
-          />} />
+              <Route path="/hobbies/fotografia" element={
 
- 
-        </Routes>
-      </main>
-      <Footer />
-    </Router>
+
+                <HobbiesPost
+                  title={dataFotografia.title}
+                  content={dataFotografia.content}
+                  img_url={dataFotografia.img_url}
+                  video_url_pg={dataFotografia.video_url_pg}
+                  arial_label={dataFotografia.arial_label}
+                  galleryItems={dataFotografia.galleryItems}
+                  nextLink={dataFotografia.nextLink}
+                  previousLink={dataFotografia.previousLink}
+                />
+              } />
+
+              <Route path="/hobbies/senderismo" element={
+
+                <HobbiesPost
+                  title={dataSenderismo.title}
+                  content={dataSenderismo.content}
+                  img_url={dataSenderismo.img_url}
+                  video_url_pg={dataSenderismo.video_url_pg}
+                  arial_label={dataSenderismo.arial_label}
+                  galleryItems={dataSenderismo.galleryItems}
+                  nextLink={dataSenderismo.nextLink}
+                  previousLink={dataSenderismo.previousLink}
+                />
+              } />
+
+              <Route path="/hobbies/videojuegos" element={<HobbiesPost
+                title={dataVideojuegos.title}
+                content={dataVideojuegos.content}
+                img_url={dataVideojuegos.img_url}
+                video_url_pg={dataVideojuegos.video_url_pg}
+                arial_label={dataVideojuegos.arial_label}
+                galleryItems={dataVideojuegos.galleryItems}
+                nextLink={dataVideojuegos.nextLink}
+                previousLink={dataVideojuegos.previousLink}
+              />} />
+
+
+            </Routes>
+          </main>
+          <Footer />
+
+        </div>
+      </Content>
+
+
+    </Layout>
+
   );
 }
 
